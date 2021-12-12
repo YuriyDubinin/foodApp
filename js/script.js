5d2c119089b61c2c14e4f43bd834cf5f263f09f5
Yura <yuradubinin100@gmail.com>
2021-12-12 17:14:17 +0700

    add timer functionality

--- js/script.js
+++ js/script.js
@@ -1,14 +1,12 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", () => {
+    /* tabs */
+
     //variables
     const tabs = document.querySelectorAll(".tabheader__item"),
         tabsContent = document.querySelectorAll(".tabcontent"),
         tabsParent = document.querySelector(".tabheader__items");
-
-    //execution
-    hideTabContent();
-    showTabContent();
 
     tabsParent.addEventListener("click", (event) => {
         const target = event.target;
@@ -40,4 +38,65 @@ document.addEventListener("DOMContentLoaded", () => {
         tabsContent[i].classList.remove("hide");
         tabs[i].classList.add("tabheader__item_active");
     }
+
+    //execution
+    hideTabContent();
+    showTabContent();
+
+    /* timer */
+
+    //variables
+    const deadLine = "2021-12-31";
+
+    //functions
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            seconds = Math.floor((t / 1000) % 60);
+
+        return {
+            total: t,
+            days,
+            hours,
+            minutes,
+            seconds,
+        };
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+            days = timer.querySelector("#days"),
+            hours = timer.querySelector("#hours"),
+            minutes = timer.querySelector("#minutes"),
+            seconds = timer.querySelector("#seconds"),
+            timeInterval = setInterval(updateClock, 1000);
+
+        updateClock(); //for instant update of the timer when the page is loaded
+
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    //execution
+    setClock(".timer", deadLine);
 });
