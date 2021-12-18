33f4a4525bf245de137fb672f5733781b394263c
Yura <yuradubinin100@gmail.com>
2021-12-18 14:08:03 +0700

    added a functionality in which the modal window appears 5sec after the page is loaded and if the user has scrolled tto the end of the page

--- js/script.js
+++ js/script.js
@@ -102,22 +102,36 @@ document.addEventListener("DOMContentLoaded", () => {
     //variables
     const contactBtns = document.querySelectorAll("[data-modal]"),
         modalWindow = document.querySelector(".modal"),
-        closeModalBtn = document.querySelector("[data-close]");
+        closeModalBtn = document.querySelector("[data-close]"),
+        modalTimerId = setTimeout(openModalWinndow, 5000);
 
     //functions
+    function openModalWinndow() {
+        modalWindow.classList.add("show");
+        modalWindow.classList.remove("hide");
+        document.body.style.overflow = "hidden";
+        clearInterval(modalTimerId);
+    }
+
     function closeModalWindow() {
         modalWindow.classList.add("hide");
         modalWindow.classList.remove("show");
         document.body.style.overflow = "";
     }
 
+    function showModalByScroll() {
+        /* In this construction below,we subtract 1px from document.documentElement.scrollheight in 
+        order to avoid bugs related to the technical characteristics of browsers and some monitors */
+
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {
+            openModalWinndow();
+            removeEventListener("scroll", showModalByScroll);
+        }
+    }
+
     //execution
     contactBtns.forEach((item) => {
-        item.addEventListener("click", (event) => {
-            modalWindow.classList.add("show");
-            modalWindow.classList.remove("hide");
-            document.body.style.overflow = "hidden";
-        });
+        item.addEventListener("click", openModalWinndow);
     });
 
     closeModalBtn.addEventListener("click", () => {
@@ -135,4 +149,6 @@ document.addEventListener("DOMContentLoaded", () => {
             closeModalWindow();
         }
     });
+
+    window.addEventListener("scroll", showModalByScroll);
 });
