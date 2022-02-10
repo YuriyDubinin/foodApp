ce35dbaf086e4c5a7c63a9de8106ba5c0cb1567c
Yura Dubinin <yuradubinin100@gmail.com>
2022-02-10 21:44:08 +0700

    Cards section minor changes
    * Receiving cards from the server now done using function getResource
    
    * Axios no longer used

--- js/script.js
+++ js/script.js
@@ -206,18 +206,18 @@ document.addEventListener("DOMContentLoaded", () => {
 
     //execution
     //add cards using the getResource
-    // getResource("http://localhost:300gi0/menu").then((data) => {
-    //     data.forEach(({ img, altimg, title, descr, price }) => {
-    //         new MenuCard(img, altimg, title, descr, price, ".menu .container").render();
-    //     });
-    // });
-
-    //add cards using axios
-    axios.get("http://localhost:3000/menu").then((data) => {
-        data.data.forEach(({ img, altimg, title, descr, price }) => {
+    getResource("http://localhost:3000/menu").then((data) => {
+        data.forEach(({ img, altimg, title, descr, price }) => {
             new MenuCard(img, altimg, title, descr, price, ".menu .container").render();
         });
     });
+
+    //add cards using axios
+    // axios.get("http://localhost:3000/menu").then((data) => {
+    //     data.data.forEach(({ img, altimg, title, descr, price }) => {
+    //         new MenuCard(img, altimg, title, descr, price, ".menu .container").render();
+    //     });
+    // });
 
     /* Forms */
     //declaration
